91d92a49a4b876972f7a6170c89df2a51c3434c5
Mauricio Reina <jose.reina@funiber.org>
2017-09-13 16:59:59 -0500

    Added task to compile css
    Added library for handler css compile from sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     pump = require('pump'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     qunit = require('gulp-qunit');
 
@@ -16,6 +18,25 @@ gulp
             ],
             cb
         );
+    })
+
+    .task('css-prefix', function (cb) {
+        gulp.src('src/selectbox.css')
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('dist'));
+    })
+
+    .task('sass', function () {
+        return gulp.src('./src/selectbox.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('./dist/css'));
+    })
+
+    .task('sass:watch', function () {
+        gulp.watch('./src/selectbox.scss', ['sass']);
     })
 
     .task('test', function() {

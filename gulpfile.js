21b69924f9f3faedf205142c392568a42716da0a
Mauricio Reina <jose.reina@funiber.org>
2017-09-18 10:59:39 -0500

    It added linter JSHint
    It added linter JSHint and task of Gulp for execute process
    of JSHint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,80 @@
 var gulp = require('gulp'),
     pump = require('pump'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sass = require('gulp-sass'),
+
+    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    qunit = require('gulp-qunit');
+    qunit = require('gulp-qunit'),
+
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass');
 
 gulp
+    /**
+     * Default task test.
+     */
     .task('default', function () {
         console.log('Test default Gulp!');
     })
 
+
+    /**
+     * Verify syntax all files of the path ./src/js.
+     */
+    .task('lint', function() {
+        return gulp.src('./src/js/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter("jshint-stylish"));
+    })
+
+
+    /**
+     * Minification for files js.
+     */
     .task('compress', function (cb) {
         pump([
-                gulp.src('src/*.js'),
+                gulp.src('./src/*.js'),
                 uglify(),
-                gulp.dest('dist')
+                gulp.dest('./dist')
             ],
             cb
         );
     })
 
-    .task('css-prefix', function (cb) {
-        gulp.src('src/selectbox.css')
-            .pipe(autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            }))
-            .pipe(gulp.dest('dist'));
-    })
 
+    /**
+     * Interpreter for sass-lang.
+     */
     .task('sass', function () {
         return gulp.src('./src/selectbox.scss')
             .pipe(sass().on('error', sass.logError))
             .pipe(gulp.dest('./dist/css'));
     })
 
+
+    /**
+     * Observer for compilation form Sass-lang to CSS.
+     */
     .task('sass:watch', function () {
         gulp.watch('./src/selectbox.scss', ['sass']);
     })
 
+
+    /**
+     * Task for add prefix necessary for multiples browsers.
+     */
+    .task('css-prefix', function (cb) {
+        gulp.src('./src/scss/selectbox.scss')
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('./dist'));
+    })
+
+
+    /**
+     * Execute all test.
+     */
     .task('test', function() {
         return gulp.src('./test/index.html')
             .pipe(qunit());
